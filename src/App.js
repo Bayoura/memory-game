24cf6a0ebe94bd653675ce7aeebf3a705e4b5d11
Bayoura <kristin.seidelmann@gmx.de>
2022-08-08 20:03:49 +0200

    add state and shuffle function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import CardGrid from "./components/CardGrid";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  const newGame = {
+    cards: [
+      {
+        name: "1",
+        id: uuidv4(),
+        clicked: false,
+      },
+      {
+        name: "2",
+        id: uuidv4(),
+        clicked: false,
+      },
+      {
+        name: "3",
+        id: uuidv4(),
+        clicked: false,
+      },
+      {
+        name: "4",
+        id: uuidv4(),
+        clicked: false,
+      },
+      {
+        name: "5",
+        id: uuidv4(),
+        clicked: false,
+      },
+    ],
+    currentScore: 0,
+    highScore: 0,
+  };
+
+  const [game, setGame] = useState(newGame);
+
+  //fisher-yates shuffle
+  function shuffle() {
+    const newCards = [...game.cards];
+    for (let i = newCards.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+
+      [newCards[i], newCards[j]] = [newCards[j], newCards[i]];
+    }
+    setGame((current) => {
+      return {
+        ...current,
+        cards: newCards,
+      };
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Memory Game</h1>
       </header>
+      <div>
+        <CardGrid cards={game.cards} shuffle={shuffle} />
+      </div>
     </div>
   );
 }
