45a38a20cee3cfe09053b23047a7f6c7de39c7bf
Bayoura <kristin.seidelmann@gmx.de>
2022-08-12 19:37:20 +0200

    import fonts

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import "./fonts/pokemon-gb-font/PokemonGb-Regular.ttf";
+import "./fonts/pokemon/Pokemon-Hollow.ttf";
+import "./fonts/pokemon/Pokemon-Solid.ttf";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
